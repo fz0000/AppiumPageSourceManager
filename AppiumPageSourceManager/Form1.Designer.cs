4bf5ecffe28379e7b6c1d0e915b410432cd98dfc
fz0000 <fz0000@sina.com>
2025-02-24 17:58:57 +0800

    load from xml file

---
+++ AppiumPageSourceManager/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace AppiumPageSourceManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ToolStripButtonLoad = new System.Windows.Forms.ToolStripButton();
+            this.ToolStripButtonSave = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStripContainer1.ContentPanel.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1181, 736);
+            this.splitContainer1.SplitterDistance = 393;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(393, 736);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView1_AfterSelect);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeight = 34;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(784, 736);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Controls.Add(this.splitContainer1);
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(1181, 736);
+            this.toolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStripContainer1.Location = new System.Drawing.Point(0, 0);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(1181, 774);
+            this.toolStripContainer1.TabIndex = 1;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.toolStrip1);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ToolStripButtonLoad,
+            this.ToolStripButtonSave});
+            this.toolStrip1.Location = new System.Drawing.Point(4, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(86, 38);
+            this.toolStrip1.TabIndex = 0;
+            // 
+            // ToolStripButtonLoad
+            // 
+            this.ToolStripButtonLoad.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripButtonLoad.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripButtonLoad.Image")));
+            this.ToolStripButtonLoad.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripButtonLoad.Name = "ToolStripButtonLoad";
+            this.ToolStripButtonLoad.Size = new System.Drawing.Size(34, 33);
+            this.ToolStripButtonLoad.Text = "打开(&O)";
+            this.ToolStripButtonLoad.Click += new System.EventHandler(this.ToolStripButtonLoad_Click);
+            // 
+            // ToolStripButtonSave
+            // 
+            this.ToolStripButtonSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ToolStripButtonSave.Image = ((System.Drawing.Image)(resources.GetObject("ToolStripButtonSave.Image")));
+            this.ToolStripButtonSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ToolStripButtonSave.Name = "ToolStripButtonSave";
+            this.ToolStripButtonSave.Size = new System.Drawing.Size(34, 28);
+            this.ToolStripButtonSave.Text = "保存(&S)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1181, 774);
+            this.Controls.Add(this.toolStripContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStripContainer1.ContentPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton ToolStripButtonLoad;
+        private System.Windows.Forms.ToolStripButton ToolStripButtonSave;
+    }
+}
+
---
+++ AppiumPageSourceManager/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace AppiumPageSourceManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            SetProcessDpiAwareness();
+            InitializeComponent();
+        }
+
+        private static void SetProcessDpiAwareness()
+        {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                try
+                {
+                    SetProcessDpiAwareness(PROCESS_DPI_AWARENESS.Process_Per_Monitor_DPI_Aware);
+                }
+                catch
+                {
+                    SetProcessDPIAware();
+                }
+            }
+        }
+
+
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
+        [DllImport("shcore.dll")]
+        private static extern int SetProcessDpiAwareness(PROCESS_DPI_AWARENESS awareness);
+
+        private enum PROCESS_DPI_AWARENESS
+        {
+            Process_DPI_Unaware = 0,
+            Process_System_DPI_Aware = 1,
+            Process_Per_Monitor_DPI_Aware = 2
+        }
+
+        private void ToolStripButtonLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "XML Files|*.xml",
+                Title = "Select Appium PageSource XML File"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                LoadXmlToTree(openFileDialog.FileName);
+            }
+        }
+
+        private void LoadXmlToTree(string filePath)
+        {
+            treeView1.Nodes.Clear();
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filePath);
+            TreeNode rootNode = CreateNode(xmlDoc.DocumentElement);
+            treeView1.Nodes.Add(rootNode);
+            AddNodes(xmlDoc.DocumentElement, rootNode);
+            treeView1.CollapseAll();
+        }
+
+        private TreeNode CreateNode(XmlNode xmlNode)
+        {
+            string localizedControlType = xmlNode.Attributes?["LocalizedControlType"]?.Value ?? "";
+            string name = xmlNode.Attributes?["Name"]?.Value ?? "";
+            string autoid = xmlNode.Attributes?["AutomationId"]?.Value ?? "";
+            string nodeName = $"\"{localizedControlType}\" \"{name}\" \"{autoid}\"";
+            return new TreeNode(nodeName) { Tag = xmlNode };
+        }
+
+        private void AddNodes(XmlNode xmlNode, TreeNode treeNode)
+        {
+            foreach (XmlNode childNode in xmlNode.ChildNodes)
+            {
+                TreeNode childTreeNode = CreateNode(childNode);
+                treeNode.Nodes.Add(childTreeNode);
+                AddNodes(childNode, childTreeNode);
+            }
+        }
+
+        private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Tag is XmlNode xmlNode)
+            {
+                dataGridView1.Columns.Clear();
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Add("Attribute", "Attribute");
+                dataGridView1.Columns.Add("Value", "Value");
+                if (xmlNode.Attributes != null)
+                {
+                    foreach (XmlAttribute attr in xmlNode.Attributes)
+                    {
+                        dataGridView1.Rows.Add(attr.Name, attr.Value);
+                    }
+                }
+            }
+        }
+    }
+}
